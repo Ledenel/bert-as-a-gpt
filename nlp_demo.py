16c6c0da20a2038dd65bbcdb31d8ce651e67db12
Ledenel <ledenelintelli@gmail.com>
2021-01-31 01:59:56 +0800

    add something like spell cheker

--- nlp_demo.py
+++ nlp_demo.py
@@ -4,6 +4,9 @@ import os
 import torch
 import pprint
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 from torch.nn.functional import log_softmax
 
 config_dict = dict(
@@ -26,17 +29,28 @@ tokenizer, model = init()
 
 sequence = st.text_input("Sentence", value=f"生活的真谛是美")
 
-input = tokenizer.encode(sequence, return_tensors="pt")
-for mask_token_index, real_token in enumerate(input[0]):
+import matplotlib.pyplot as plt
+
+with torch.no_grad():
+    sequence_input = tokenizer.encode(sequence, return_tensors="pt")
+    mask_token_index, real_token = list(enumerate(sequence_input[0]))[1]
     if tokenizer.convert_ids_to_tokens([real_token])[0] not in tokenizer.all_special_tokens:
-        masked_input = input.clone()
-        # masked_input[0, mask_token_index] = tokenizer.mask_token_id
+        masked_input = sequence_input.clone()
+        masked_input[0, mask_token_index] = tokenizer.mask_token_id
         print(tokenizer.decode(masked_input[0]))
         print(tokenizer.convert_ids_to_tokens([real_token])[0])
         token_logits = model(masked_input).logits
-        mask_token_logits = log_softmax(token_logits[0, mask_token_index, :])
-        top_5_tokens = torch.topk(mask_token_logits, 5).indices.tolist()
-        st.text(
-            ",".join(f"{print_token(x)}:{float(mask_token_logits[x])}" for x in [real_token] + top_5_tokens)
-        )
-    
+        print(token_logits[0, :, :].shape)
+        mask_token_logits = -log_softmax(token_logits[0, :, :], dim=1)
+        print(mask_token_logits.shape)
+        df_logits = mask_token_logits.detach().numpy()
+        actual_words = sequence_input[0][1:-1]
+        df_logits = df_logits.T[actual_words].T
+        df_logits = pd.DataFrame(df_logits, columns=tokenizer.convert_ids_to_tokens(actual_words))
+        st.write(df_logits)
+        # st.plotly_chart(go.Figure(data=go.Heatmap(z=df_logits)))
+        
+        # st.line_chart({
+
+        # })
+        
